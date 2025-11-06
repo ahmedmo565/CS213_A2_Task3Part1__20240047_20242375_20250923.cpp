2c9c157815a2af46bcabfed3a6f10e2360c0b7a1
AhmedNasser <medo1832006@gmail.com>
2025-11-07 00:42:10 +0200

    Update playerGui.h

--- playerGui.h
+++ playerGui.h
@@ -1,11 +1,12 @@
- #pragma once
+#pragma once
 #include <JuceHeader.h>
-#include "playerAudio.h"
-#include "playerGui.h"
+#include "PlayerAudio.h"
 #include "WaveForm.h"
+
 class PlayerGUI : public juce::Component,
     public juce::Button::Listener,
-    public juce::Slider::Listener
+    public juce::Slider::Listener,
+    public juce::Timer
 {
 public:
     PlayerGUI();
@@ -13,51 +14,54 @@ public:
 
     void resized() override;
     void paint(juce::Graphics& g) override;
+    void timerCallback() override;
 
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
     void releaseResources();
 
+    void applyTheme();
+
 private:
     PlayerAudio playerAudio;
 
-    
     juce::TextButton loadButton{ "Load Files" };
     juce::TextButton restartButton{ "Restart" };
     juce::TextButton stopButton{ "Stop" };
     juce::TextButton gotostartButton{ "start" };
     juce::TextButton muteButton{ "Mute" };
     juce::TextButton repeatButton{ "repeat: off" };
-
-    
+    juce::TextButton aButton{ "Set a" };
+    juce::TextButton bButton{ "Set b" };
+    juce::TextButton clearabButton{ "Clear a&b" };
     juce::TextButton prevButton{ "Previous" };
     juce::TextButton nextButton{ "Next" };
     juce::TextButton clearPlaylistButton{ "Clear Playlist" };
 
     juce::Slider volumeSlider;
     juce::Slider speedSlider;
+    juce::Slider positionSlider;
+    juce::Label timeLabel;
 
-    
     juce::Label metadataLabel;
     juce::TextEditor metadataDisplay;
-
-    
     juce::Label playlistLabel;
     juce::ListBox playlistBox;
 
+    juce::String formatTime(double seconds);
+
     std::unique_ptr<juce::FileChooser> fileChooser;
-
     bool isRepeating = false;
+    double abStart = 0.0;
+    double abEnd = 0.0;
 
-  
+    WaveForm waveForm;
+
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
-    WaveForm waveForm;
-   
     void updateMetadataDisplay();
     void updatePlaylist();
 
-    
     class PlaylistModel : public juce::ListBoxModel
     {
     public:
@@ -71,17 +75,18 @@ private:
         void paintListBoxItem(int rowNumber, juce::Graphics& g,
             int width, int height, bool rowIsSelected) override
         {
+            auto theme = owner.playerAudio.getCurrentTheme();
             if (rowIsSelected)
-                g.fillAll(juce::Colours::lightblue);
+                g.fillAll(theme.buttonColour);
             else
-                g.fillAll(juce::Colours::white);
+                g.fillAll(theme.playlistBackground);
 
-            g.setColour(juce::Colours::black);
+            g.setColour(theme.playlistText);
             g.setFont(14.0f);
 
             juce::String itemText = owner.playerAudio.getPlaylistItemName(rowNumber);
             if (rowNumber == owner.playerAudio.getCurrentPlaylistIndex())
-                itemText = "▶ " + itemText;
+                itemText = "> " + itemText;
 
             g.drawText(itemText, 4, 0, width - 4, height, juce::Justification::centredLeft);
         }
@@ -107,4 +112,3 @@ private:
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
 };
-
